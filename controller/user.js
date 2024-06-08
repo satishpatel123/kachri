ba586a5e4739c3aa14cd9f37f2a79784787b9aa2
lakhtariya satish i <34060391+satishpatel123@users.noreply.github.com>
2024-06-08 21:12:09 +0530

    update api some issue

--- controller/user.js
+++ controller/user.js
@@ -144,24 +144,37 @@ exports.loginUser = async (req, res, next) => {
       res.status(400).send("Please enter mobile number and password");
     }
     const user = await User.findOne({ phoneNumber });
-    if(user.password) {
-      if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign(
-          { user_id: user._id, phoneNumber, email: user.email },
-          "hFB4rzSIjqoclVvIANXF5Fj8QWG6GOW6",
-          {
-            expiresIn: "10h",
-          }
-        );
-        return res.status(200).json({
-          data: user,
-          token: token,
-          message: "User has been login successfully ",
+    if(user) {
+      if (user.password) {
+        if((await bcrypt.compare(password, user.password))) {
+          const token = jwt.sign(
+            { user_id: user._id, phoneNumber, email: user.email },
+            "hFB4rzSIjqoclVvIANXF5Fj8QWG6GOW6",
+            {
+              expiresIn: "10h",
+            }
+          );
+          return res.status(200).json({
+            data: user,
+            token: token,
+            message: "User has been login successfully ",
+          });
+        } else {
+          return res.status(400).send({
+            message: "Invalid phone number and password.",
+            status: false,
+          });
+        }
+        
+      } else {
+        return res.status(400).send({
+          message: "You are login another role.",
+          status: false,
         });
       }
     } else {
       return res.status(400).send({
-        message: "You are login another role.",
+        message: "Invalid phone number.",
         status: false,
       });
     }
